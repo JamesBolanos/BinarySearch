d3e63d03042f6c023c74aa4a17539ca1df65e684
Jaime Bolanos <jbolanosdiaz@gmail.com>
2021-01-24 11:18:05 -0500

    the class now uses a recursive approach, so it calls itself

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -2,31 +2,29 @@ public class BinarySearch {
 
     int binarySearch(int[] array, int x, int low, int high) {
 
-        int mid;
-        int returnValue = -1;
-        while (high >= low) {
+        int mid = -1;
+        if (high >= low) {
             mid = (low + high) / 2;
             if (x == array[mid]) {
-                returnValue = mid;
-                break;
+                return mid;
             } else if (x < array[mid]) {
-                high = mid - 1;
+                mid = binarySearch(array, x, low, mid - 1);
             } else {
-                low = mid + 1;
+                mid = binarySearch(array, x, mid + 1, high);
             }
         }
-        return returnValue;
+        return mid;
     }
 
     public static void main(String[] args) {
 
         int[] array = {2, 4, 6, 8, 10, 12};
         BinarySearch binarySearch = new BinarySearch();
-        int result = binarySearch.binarySearch(array, 4,0,array.length);
-        if (result == -1){
+        int result = binarySearch.binarySearch(array, 4, 0, array.length);
+        if (result == -1) {
             System.out.println("The Element is not found in the array");
-        }else {
-            System.out.println("The Element is found in the array at index "+result);
+        } else {
+            System.out.println("The Element is found in the array at index " + result);
         }
     }
 }
